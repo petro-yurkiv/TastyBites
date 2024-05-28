cfcefbee38bada0704184cf7ceaead6312548c5f
Ptero Yurkiv <petroyur12@gmail.com>
2024-05-28 13:45:36 -0700

    recipes refactor, add recipe view

---
+++ TastyBites/AddRecipe/AddRecipeTextField.swift
@@ -0,0 +1,18 @@
+//
+//  AddRecipeTextField.swift
+//  TastyBites
+//
+//  Created by Petro Yurkiv on 28.05.2024.
+//
+
+import SwiftUI
+
+struct AddRecipeTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRecipeTextField()
+}
